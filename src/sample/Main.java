d8019a9394c27772eea5f71f785e389b89268138
Jarek <jjankowski1@gmail.com>
2018-10-08 22:49:08 +0200

    first commit

---
+++ src/sample/Main.java
@@ -0,0 +1,17 @@
+package sample;
+
+import javafx.application.Application;
+
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        BorderPane layout = new BorderPane();
+
+
+    }
+}
